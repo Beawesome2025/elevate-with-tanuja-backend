b7c9aac5b9497806ba45020bdd96c784e2b7d045
Subhajit Mandal <subhajitm@Subhajits-MacBook-Air.local>
2026-02-27 22:06:54 +0530

    Fix: Corrected database confi syntax

--- config/database.ts
+++ config/database.ts
@@ -6,21 +6,14 @@ export default ({ env }) => {
   return {
     connection: {
       client: 'postgres',
-      connection: connectionString
-        ? {
-            connectionString,
-            ssl: { rejectUnauthorized: false },
-          },
-          acquireConnectionTimeout: 60000,},
-        : {
-            host: env('DATABASE_HOST', '127.0.0.1'),
-            port: env.int('DATABASE_PORT', 5432),
-            database: env('DATABASE_NAME', 'neondb'), // Force it to neondb
-            user: env('DATABASE_USERNAME', 'neondb_owner'),
-            password: env('DATABASE_PASSWORD'),
-            ssl: { rejectUnauthorized: false },
-          },
-      debug: false,
+      connection: {
+        connectionString,
+        ssl: connectionString ? { rejectUnauthorized: false } : false,
+      },
+      pool: {
+        min: env.int('DATABASE_POOL_MIN', 2),
+        max: env.int('DATABASE_POOL_MAX', 10),
+      },
     },
   };
 };
